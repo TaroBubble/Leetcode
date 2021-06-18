b967015aeeedb3f2f5ab731ac36cd160a82e6826
Kevin <kchen0296@gmail.com>
2021-06-18 09:57:45 -0400

    problems

---
+++ Linked List/MergeTwoSortedLists.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  let head = new ListNode();
+  let curr = head;
+  let p1 = l1;
+  let p2 = l2;
+  while (p1 !== null && p2 !== null) {
+    if (p1.val < p2.val) {
+      curr.next = p1;
+      p1 = p1.next;
+    } else {
+      curr.next = p2;
+      p2 = p2.next;
+    }
+    curr = curr.next;
+  }
+  if (p1 === null) curr.next = p2;
+  if (p2 === null) curr.next = p1;
+  return head.next;
+};
